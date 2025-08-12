053d5227219e7b84e69d5d6ae06896e803ea4f75
HoaiMinh <quangluatlm@gmail.com>
2025-08-12 21:01:15 +0700

    Adds initial project setup with Maven Wrapper
    Sets up a new Spring Boot project.
    
    Configures the project with a Maven Wrapper for consistent builds.
    Also, adds .gitignore and .gitattributes for a clean git repository.

---
+++ src/main/java/org/example/hoc_java/HocJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.hoc_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HocJavaApplication.class, args);
+    }
+
+}
